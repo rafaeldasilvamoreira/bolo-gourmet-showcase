a77b997033131ed828288d5ff43c2a99afb5e65f
Rafael Moreira <contato.devrafael@gmail.com>
2025-10-24 16:27:19 -0300

    altera icone boneco

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import About from "@/components/About";
 import Products from "@/components/Products";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
-import BonecoMP4 from "@/assets/boneco.mp4";
+
 
 const Index = () => {
   return (
