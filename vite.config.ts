fc4c90f33c47df414e4b40c08a3e3c5d5ca14b0b
Rafael Moreira <contato.devrafael@gmail.com>
2025-10-24 16:23:44 -0300

    alterar estrutura para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-
 export default defineConfig(({ mode }) => ({
   base: '/bolo-gourmet-showcase/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  server: { host: "::", port: 8080 },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } }
 }));
